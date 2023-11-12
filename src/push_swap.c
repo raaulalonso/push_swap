7c37a0bb0a1c6bfedb8b7326329870585d4700ca
Raúl Alonso Pardo <raul@Rauls-MacBook-Air.local>
2023-11-12 16:21:38 +0100

    many_errors

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: raalonso <raalonso@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 21:39:02 by raalonso          #+#    #+#             */
-/*   Updated: 2023/11/11 20:29:19 by raalonso         ###   ########.fr       */
+/*   Updated: 2023/11/12 15:01:59 by raalonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,6 @@ node_t	*new_node(int data)
 	return (new_node);
 }
 
-void	printnode(node_t *head)
-{
-	node_t	*tmp;
-
-	tmp = head;
-	while (tmp)
-	{
-		ft_printf("%d", tmp->data);
-		tmp = tmp->next;
-	}
-	ft_printf("\n");
-}
-
 /*Creates a list made up with all of the arguments transformed into an int.*/
 node_t	*create_list(int argc, char **argv)
 {
@@ -53,18 +40,6 @@ node_t	*create_list(int argc, char **argv)
 		argc--;
 	}
 	return (head);
-}
-
-int	calculate_price(int i, int j, node_t *head_a, node_t *head_b)
-{
-	int	price;
-
-	if (j > ft_lstsize(head_b) / 2)
-		j = (ft_lstsize(head_b) - j) + 1;
-	if (i > ft_lstsize(head_a) / 2) 
-		i = (ft_lstsize(head_a) - i) + 1;
-	price = i + j;
-	return (price);
 }
 
 /* 1 for rotate, 2 for reverse. */
@@ -110,31 +85,160 @@ void	insert_into_b(int movements, int f, node_t **head_a, node_t **head_b)
 	}
 }
 
+node_t	*get_cheaper(node_t **head_a, node_t **head_b, int max_b, int min_b)
+{
+	node_t	*current_a;
+	node_t	*current_b;
+	node_t	*cheaper;
+	int		movements;
+	int		price;
+	int		i;
+	
+	i = 0;
+	price = 0;
+	movements = 0;
+	current_a = *head_a;
+	current_b = *head_b;
+	while (current_a != NULL)
+	{
+		if (current_a->data > max_b || current_a->data < min_b)
+		{
+			while (current_b != NULL && current_a->data != max_b)
+			{
+				current_b = current_b->next;
+				movements++;
+			}
+		}
+		else
+		{
+			while (current_b != NULL && current_a->data < current_b->data)
+			{
+				current_b = current_b->next;
+				movements++;
+			}
+		}
+		if (movements > ft_lstsize(*head_b) / 2)
+			movements = ft_lstsize(*head_b) - movements;
+		//printf("movements -> %d %d\n", movements, i);
+		if (movements + i < price || price == 0)
+		{
+			price = movements + i + 1;
+			cheaper = current_a;
+		}
+		movements = 0;
+		i++;
+		current_b = *head_b;
+		current_a = current_a->next;
+	}
+	//printf ("cheaper -> %d\n", cheaper->data);
+	return (cheaper);
+}
+
+void	move_cheaper(node_t **head_a, node_t *cheaper)
+{
+	int		movements;
+	node_t	*current_a;
+	int		f;
+
+	f = 0;
+	movements = 0;
+	current_a = *head_a;
+	while (current_a != cheaper)
+	{
+		current_a = current_a->next;
+		movements++;
+	}
+	if (movements > ft_lstsize(*head_a) / 2)
+	{
+		movements = ft_lstsize(*head_a) - movements;
+		f = 1;
+	}
+	while (movements != 0)
+	{
+		if (f == 1)
+			rev_rotate(&*head_a, 1);
+		else
+			rotate(&*head_a, 1);
+		movements--;
+	}
+}
+
+void	put_above_max(node_t **head_a, node_t **head_b, int max_b)
+{
+	int		movements;
+	node_t	*current_b;
+	int		f;
+
+	f = 0;
+	movements = 0;
+	current_b = *head_b;
+	while (current_b->data != max_b)
+	{
+		current_b = current_b->next;
+		movements++;
+	}
+	if (movements > ft_lstsize(*head_b) / 2)
+	{
+		movements = ft_lstsize(*head_b) - movements;
+		f = 1;
+	}
+	while (movements != 0)
+	{
+		if (f == 1)
+			rev_rotate(&*head_b, 2);
+		else
+			rotate(&*head_b, 2);
+		movements--;
+	}
+	push(&*head_a, &*head_b, 2);
+}
+
 void	big_sort(node_t **head_a, node_t **head_b)
 {	
 	node_t	*current_a;
 	node_t	*current_b;
+	node_t	*cheaper;
 	int		movements;
 	int		count;
 	int		lst_size;
 	int		f;
+	int		max_b;
+	int		min_b;
 
+	max_b = (*head_b)->data;
+	min_b = (*head_b)->next->data;
 	count = 0;
 	lst_size = ft_lstsize(*head_a);
 	while (count < lst_size)
 	{
-		f = 0;
 		movements = 0;
-		current_a = *head_a;
 		current_b = *head_b;
-		while (current_b != NULL && current_a->data < current_b->data)
+		current_a = *head_a;
+		//cheaper = get_cheaper(&*head_a, &*head_b, max_b, min_b);
+		//move_cheaper(&*head_a, cheaper);
+		f = 0;
+		current_a = *head_a;
+		/*if (current_a->data > max_b)
 		{
-			current_b = current_b->next;
-			movements++;
+			put_above_max(&*head_a, &*head_b, max_b);
+			max_b = current_a->data;
 		}
-		if (movements > ft_lstsize(*head_b) / 2)
-			f = 1;
-		insert_into_b(movements, f, &*head_a, &*head_b);
+		else if (current_a->data < min_b)
+		{
+			put_above_max(&*head_a, &*head_b, max_b);
+			min_b = current_a->data;
+		}*/
+		//else
+		//{
+			while (current_b != NULL && current_a->data < current_b->data)
+			{
+				current_b = current_b->next;
+				movements++;
+			}
+			if (movements > ft_lstsize(*head_b) / 2)
+				f = 1;
+			insert_into_b(movements, f, &*head_a, &*head_b);
+		//}
 		count++;
 	}
 	count = 0;
